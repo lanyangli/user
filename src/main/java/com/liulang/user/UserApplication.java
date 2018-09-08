a33b9951c8df6dc86bbc619422979eedc0e3abf0
lanyangli <lanyangli@DESKTOP-80HMKA8>
2018-09-09 04:06:49 +0800

    u

--- src/main/java/com/liulang/user/UserApplication.java
+++ src/main/java/com/liulang/user/UserApplication.java
@@ -3,11 +3,7 @@ package com.liulang.user;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-/**
- * 服务注册中心
- * @author lanyangli
- *
- */
+
 @SpringBootApplication
 @EnableEurekaClient
 public class UserApplication {
